7f222b8241ea57fbe3b1f6d292d96907989f5ebd
Eric Faguy <efaguy@uoguelph.ca>
2020-03-24 20:12:45 -0400

    Updated run to support recording

--- tetris-ai/run.py
+++ tetris-ai/run.py
@@ -11,18 +11,18 @@ from tqdm import tqdm
 def dqn():
     env = Tetris()
     episodes = 2000
-    max_steps = None
+    max_steps = 50000
     epsilon_stop_episode = 1500
     mem_size = 20000
     discount = 0.95
     batch_size = 512
     epochs = 1
-    render_every = 50
+    render_every = 500
     log_every = 50
     replay_start_size = 2000
     train_every = 1
     n_neurons = [32, 32]
-    render_delay = None
+    render_delay = 0.01
     activations = ['relu', 'relu', 'linear']
 
     agent = DQNAgent(env.get_state_size(),
@@ -34,16 +34,22 @@ def dqn():
     log = CustomTensorBoard(log_dir=log_dir)
 
     scores = []
+    steps_list = []
 
     for episode in tqdm(range(episodes)):
         current_state = env.reset()
         done = False
         steps = 0
 
-        if render_every and episode % render_every == 0:
+        if (render_every and episode % render_every == 0) or episode == (episodes - 1):
             render = True
         else:
             render = False
+			
+        if episode == (episodes - 1):
+            record = True
+        else:
+            record = False
 
         # Game
         while not done and (not max_steps or steps < max_steps):
@@ -57,13 +63,14 @@ def dqn():
                     break
 
             reward, done = env.play(best_action[0], best_action[1], render=render,
-                                    render_delay=render_delay)
+                                    render_delay=render_delay, record=record)
             
             agent.add_to_memory(current_state, next_states[best_action], reward, done)
             current_state = next_states[best_action]
             steps += 1
 
         scores.append(env.get_game_score())
+        steps_list.append(steps)
 
         # Train
         if episode % train_every == 0:
@@ -74,9 +81,10 @@ def dqn():
             avg_score = mean(scores[-log_every:])
             min_score = min(scores[-log_every:])
             max_score = max(scores[-log_every:])
+            avg_steps = mean(steps_list[-log_every:])
 
             log.log(episode, avg_score=avg_score, min_score=min_score,
-                    max_score=max_score)
+                    max_score=max_score, avg_steps=avg_steps)
 
 
 if __name__ == "__main__":
